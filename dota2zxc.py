9b9a9703ef6264a8b75878854507dd273ad8f986
Daniil <shmeltsovd@gmail.comgit confir --global user.name JoKeRqZzZ>
2023-10-09 13:54:51 +0300

    dota2qwe

---
+++ dota2zxc.py
@@ -0,0 +1,219 @@
+heroes = {
+    'Abaddon': {"Primary Attribute": "Universal", "Role": ["pos4", "pos5"]},
+    'Alchemist': {"Primary Attribute": "Strength", "Role": ["pos1", "pos2", "pos3"]},
+    "Ancient Apparition":{"Primary Attribute": "Intelligence", "Role": ["pos4", "pos5"]},
+    'Anti-Mage':{"Primary Attribute": "Agility", "Role": ["pos1"]},
+    'Arc Warden':{"Primary Attribute": "Agility", "Role": ["pos1","pos2"]},
+    "Axe":{"Primary Attribute": "Strength", "Role": ["pos3"]},
+    'Bane':{"Primary Attribute": "Universal", "Role": ["pos4", "pos5"]},
+    'Batrider':{"Primary Attribute": "Universal", "Role": ["pos2","pos3","pos4"]},
+    'Beastmaster':{"Primary Attribute": "Universal", "Role": ["pos3"]},
+    'Bloodseeker':{"Primary Attribute": "Agility", "Role": ["pos1"]},
+    'Bounty Hunter':{"Primary Attribute": "Agility", "Role": ["pos4","pos5"]},
+    'Brewmaster':{"Primary Attribute": "Universal", "Role": ["pos3"]},
+    'Bristleback':{"Primary Attribute": "Strength", "Role": ["pos3"]},
+    'Broodmother':{"Primary Attribute": "Universal", "Role": ["pos2", "pos3"]},
+    'Centaur Warrunner':{"Primary Attribute": "Strength", "Role": ["pos3"]},
+    'Chaos Knight':{"Primary Attribute": "Strength", "Role": ["pos1", "pos3"]},
+    'Chen':{"Primary Attribute": "Universal", "Role": ["pos4","pos5"]},
+    'Clinkz':{"Primary Attribute": "Agility", "Role": ["pos4","pos5"]},
+    'Clockwerk':{"Primary Attribute": "Universal", "Role": ["pos4","pos5"]},
+    'Crystal Maiden':{"Primary Attribute": "Intelligence", "Role": ["pos4","pos5"]},
+    'Dawnbreaker': {"Primary Attribute": "Strength", "Role": ["pos3"]},
+    'Dark Seer': {"Primary Attribute": "Universal", "Role": ["pos3"]},
+    'Dark Willow': {"Primary Attribute": "Universal", "Role": ["pos4","pos5"]},
+    'Dazzle': {"Primary Attribute": "Universal", "Role": ["pos2", "pos4","pos5"]},
+    "Death Prophet": {"Primary Attribute": "Intelligence", "Role": ["pos2","pos3"]},
+    "Disruptor": {"Primary Attribute": "Intelligence", "Role": ["pos4","pos5"]},
+    "Doom": {"Primary Attribute": "Strength", "Role": ["pos2","pos3"]},
+    "Dragon Knight": {"Primary Attribute": "Strength", "Role": ["pos2", "pos3"]},
+    'Drow Ranger': {"Primary Attribute": "Agility", "Role": ["pos1"]},
+    'Earth Spirit': {"Primary Attribute": "Strength", "Role": ["pos2", "pos4"]},
+    'Earthshaker': {"Primary Attribute": "Strength", "Role": ["pos2", "pos4"]},
+    'Elder Titan': {"Primary Attribute": "Strength", "Role": ["pos3", "pos4","pos5"]},
+    'Ember Spirit': {"Primary Attribute": "Agility", "Role": ["pos2"]},
+    'Enchantress': {"Primary Attribute": "Intelligence", "Role": ["pos4","pos5"]},
+    'Enigma': {"Primary Attribute": "Universal", "Role": ["pos3"]},
+    'Faceless Void': {"Primary Attribute": "Agility", "Role": ["pos1"]},
+    'Grimstroke': {"Primary Attribute": "Intelligence", "Role": ["pos4","pos5"]},
+    'Gyrocopter': {"Primary Attribute": "Agility", "Role": ["pos1","pos4"]},
+    "Hoodwink": {"Primary Attribute": "Agility", "Role": ["pos4","pos5"]},
+    'Huskar': {"Primary Attribute": "Strength", "Role": ["pos2"]},
+    'Invoker': {"Primary Attribute": "Universal", "Role": ["pos2"]},
+    'Io': {"Primary Attribute": "Universal", "Role": ["pos2", "pos4","pos5"]},
+    'Jakiro': {"Primary Attribute": "Intelligence", "Role": ["pos4","pos5"]},
+    'Juggernaut': {"Primary Attribute": "Agility", "Role": ["pos1"]},
+    'Keeper of the Light': {"Primary Attribute": "Intelligence", "Role": ["pos2","pos4","pos5"]},
+    'Kunkka': {"Primary Attribute": "Strength", "Role": ["pos2", "pos3"]},
+    'Legion Commander': {"Primary Attribute": "Strength", "Role": ["pos3"]},
+    'Leshrac': {"Primary Attribute": "Intelligence", "Role": ["pos2"]},
+    'Lich': {"Primary Attribute": "Intelligence", "Role": ["pos4","pos5"]},
+    'Lifestealer': {"Primary Attribute": "Strength", "Role": ["pos1"]},
+    'Lina': {"Primary Attribute": "Intelligence", "Role": ["pos2"]},
+    'Lion': {"Primary Attribute": "Intelligence", "Role": ["pos4","pos5"]},
+    'Lone Druid': {"Primary Attribute": "Universal", "Role": ["pos1","pos2","pos3"]},
+    'Luna': {"Primary Attribute": "Intelligence", "Role": ["pos1"]},
+    'Lycan': {"Primary Attribute": "Universal", "Role": ["pos2","pos3"]},
+    'Magnus': {"Primary Attribute": "Universal", "Role": ["pos3"]},
+    'Mars': {"Primary Attribute": "Strength", "Role": ["pos3"]},
+    'Marsi': {"Primary Attribute": "Universal", "Role": ["pos1","pos4","pos5"]},
+    'Medusa': {"Primary Attribute": "Agility", "Role": ["pos1"]},
+    'Meepo': {"Primary Attribute": "Agility", "Role": ["pos2"]},
+    'Mirana': {"Primary Attribute": "Universal", "Role": ["pos4","pos5"]},
+    'Muerta': {"Primary Attribute": "Intelligence", "Role": ["pos1"]},
+    'Monkey King': {"Primary Attribute": "Agility", "Role": ["pos1", "pos2"]},
+    'Morphling': {"Primary Attribute": "Agility", "Role": ["pos1", "pos2"]},
+    'Naga Siren': {"Primary Attribute": "Agility", "Role": ["pos1"]},
+    'Natures Prophet': {"Primary Attribute": "Intelligence", "Role": ["pos3","pos4","pos5"]},
+    'Necrophos': {"Primary Attribute": "Intelligence", "Role": ["pos2","pos3"]},
+    'Night Stalker': {"Primary Attribute": "Strength", "Role": ["pos3"]},
+    'Nyx Assassin': {"Primary Attribute": "Universal", "Role": ["pos4"]},
+    'Ogre Magi': {"Primary Attribute": "Strength", "Role": ["pos4","pos5"]},
+    'Omniknight': {"Primary Attribute": "Strength", "Role": ["pos3","pos5"]},
+    'Oracle': {"Primary Attribute": "Intelligence", "Role": ["pos4","pos5"]},
+    'Outworld Destroyer': {"Primary Attribute": "Intelligence", "Role": ["pos2"]},
+    'Pangolier': {"Primary Attribute": "Universal", "Role": ["pos2","pos3"]},
+    'Phantom Assassin': {"Primary Attribute": "Agility", "Role": ["pos1"]},
+    'Phantom Lancer': {"Primary Attribute": "Agility", "Role": ["pos1"]},
+    'Phoenix': {"Primary Attribute": "Universal", "Role": ["pos3","pos4","pos5"]},
+    'Puck': {"Primary Attribute": "Intelligence", "Role": ["pos2"]},
+    "Primal Beast" : {"Primary Attribute": "Strength", "Role": ["pos2","pos3"]},
+    'Pudge': {"Primary Attribute": "Strength", "Role": ["pos3","pos4","pos5"]},
+    'Pugna': {"Primary Attribute": "Intelligence", "Role": ["pos2","pos4","pos5"]},
+    'Queen of Pain': {"Primary Attribute": "Intelligence", "Role": ["pos2"]},
+    'Razor': {"Primary Attribute": "Agility", "Role": ["pos1","pos3"]},
+    'Riki': {"Primary Attribute": "Agility", "Role": ["pos1"]},
+    'Rubick': {"Primary Attribute": "Intelligence", "Role": ["pos4","pos5"]},
+    'Sand King': {"Primary Attribute": "Universal", "Role": ["pos3"]},
+    'Shadow Demon': {"Primary Attribute": "Intelligence", "Role": ["pos4","pos5"]},
+    'Shadow Fiend': {"Primary Attribute": "Agility", "Role": ["pos1", "pos2"]},
+    'Shadow Shaman': {"Primary Attribute": "Intelligence", "Role": ["pos4","pos5"]},
+    'Silencer': {"Primary Attribute": "Intelligence", "Role": ["pos4","pos5"]},
+    'Skywrath Mage': {"Primary Attribute": "Intelligence", "Role": ["pos4","pos5"]},
+    'Slardar': {"Primary Attribute": "Strength", "Role": ["pos3"]},
+    'Slark': {"Primary Attribute": "Agility", "Role": ["pos1"]},
+    'Snapfire': {"Primary Attribute": "Universal", "Role": ["pos4","pos5"]},
+    'Sniper': {"Primary Attribute": "Agility", "Role": ["pos1","pos2"]},
+    'Spectre': {"Primary Attribute": "Agility", "Role": ["pos1"]},
+    'Spirit Breaker': {"Primary Attribute": "Strength", "Role": ["pos3","pos4","pos5"]},
+    'Storm Spirit': {"Primary Attribute": "Intelligence", "Role": ["pos2"]},
+    'Sven': {"Primary Attribute": "Strength", "Role": ["pos1"]},
+    'Techies': {"Primary Attribute": "Universal", "Role": ["pos4","pos5"]},
+    'Templar Assassin': {"Primary Attribute": "Agility", "Role": ["pos1", "pos2"]},
+    'Terrorblade': {"Primary Attribute": "Agility", "Role": ["pos1"]},
+    'Tidehunter': {"Primary Attribute": "Strength", "Role": ["pos3"]},
+    'Timbersaw': {"Primary Attribute": "Universal", "Role": ["pos3"]},
+    'Tinker': {"Primary Attribute": "Intelligence", "Role": ["pos2"]},
+    'Tiny': {"Primary Attribute": "Strength", "Role": ["pos2","pos4","pos5"]},
+    'Treant Protector': {"Primary Attribute": "Strength", "Role": ["pos4","pos5"]},
+    'Troll Warlord': {"Primary Attribute": "Agility", "Role": ["pos1"]},
+    'Tusk': {"Primary Attribute": "Strength", "Role": ["pos4","pos5"]},
+    'Underlord': {"Primary Attribute": "Strength", "Role": ["pos3"]},
+    'Undying': {"Primary Attribute": "Strength", "Role": ["pos5"]},
+    'Ursa': {"Primary Attribute": "Agility", "Role": ["pos1"]},
+    'Vengeful Spirit': {"Primary Attribute": "Universal", "Role": ["pos3", "pos4", "pos5"]},
+    'Venomancer': {"Primary Attribute": "Universal", "Role": ["pos3","pos4","pos5"]},
+    'Viper': {"Primary Attribute": "Agility", "Role": ["pos2","pos3"]},
+    'Visage': {"Primary Attribute": "Universal", "Role": ["pos2","pos3"]},
+    'Void Spirit': {"Primary Attribute": "Universal", "Role": ["pos2"]},
+    'Warlock': {"Primary Attribute": "Intelligence", "Role": ["pos5"]},
+    'Weaver': {"Primary Attribute": "Agility", "Role": ["pos1","pos4","pos5"]},
+    'Windranger': {"Primary Attribute": "Universal", "Role": ["pos1","pos2","pos3"]},
+    'Winter Wyvern': {"Primary Attribute": "Universal", "Role": ["pos5", "pos4", "pos3"]},
+    'Witch Doctor': {"Primary Attribute": "Intelligence", "Role": ["pos5", "pos4"]},
+    'Wraith King': {"Primary Attribute": "Strength", "Role": ["pos1"]},
+    'Zeus': {"Primary Attribute": "Intelligence", "Role": ["pos2","pos4"]}
+}
+lanes = {
+    'Top',
+    'Midlane',
+    'Bottom',
+}
+items = {
+    'Healing salve',
+    'Iron Breanch',
+    'Clarity',
+    'Mango',
+    'Headress',
+    'Sage Mask',
+    'Ring of Basillius'
+    'Mantle of Intell',
+    'Observer Ward ',
+    'Sentry Ward ',
+    'Blood Grenade ',
+    'Smoke of Deceit ',
+    'Faerie Fire',
+    'Tango',
+    'Ring of Protection',
+    'Ring of Regen',
+    'Circlet',
+    'Infused Raindrops',
+    'Fluffy Hat ',
+    'Wind Lace ',
+    'Orb of Venom ',
+    'Blight Stone',
+    'Crown ',
+    'Gloves of Haste',
+    'Boots of Speed ',
+
+
+
+}
+
+def Start_game():
+    print ("Добро пожаловать в дотку, выберите героя :")
+    print("Список героев: ")
+    choise_hero()
+    print ("выберите линию на которую вы пойдете:")
+    choise_lane()
+    print("Выберите стартовый закуп")
+    print("Список предметов: ")
+    choice_items()
+def characteristics_of_heroes(hero_name):
+    if hero_name in heroes:
+        hero_stats = heroes[hero_name]
+        primary_attribute = hero_stats["Primary Attribute"]
+        roles =",\t".join(hero_stats["Role"])
+
+        print(f"Характеристики героя {hero_name}:")
+        print(f"Primary Attribute:{primary_attribute}")
+        print(f"Роли:{roles}")
+
+def choise_hero():
+    for hero in heroes:
+        print(hero)
+    while(True):
+        choisehero=str(input("Выбор героя : "))
+        if (choisehero in heroes):
+            characteristics_of_heroes(choisehero)
+            break
+        else:
+            print("нет такого")
+
+def choise_lane():
+    for lane in lanes:
+        print(lane)
+    while(True):
+        choiselane=str(input("Ваша линия:" ))
+        if (choiselane in lanes):
+            print(choiselane)
+            break
+        else:
+            print("нет такой линии")
+
+def choice_items():
+    for item in items:
+        print(item)
+    choices = []
+    while True:
+        choiseitem = input("Вы возьмете: ")
+        if choiseitem in items:
+            choices.append(choiseitem)
+        else:
+            print("Неверный выбор")
+        more_choices = input("Хотите выбрать еще предметы? (y/n): ")
+        if more_choices.lower() == "n":
+            break
+    print(choices)
+
+Start_game()
